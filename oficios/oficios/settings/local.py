a3d256be8bb52dcdd90db02fc65d32d7d0ac4960
fealegre <fealegre@hotmail.com>
2020-08-29 23:16:06 -0300

    Primer commit base

---
+++ oficios/oficios/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE':'sql_server.pyodbc',
+        'NAME':'Oficios',
+        'Trusted_Connection':'yes',
+        'HOST':'FERNANDO-PC\SQLEXPRESS',
+        'OPTIONS':{
+            'driver':'SQL Server Native Client 11.0',
+        }
+    },
+}
