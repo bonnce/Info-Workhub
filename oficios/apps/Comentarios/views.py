355ca9d729ce914fda68ed1043ee8138a970c199
bonnce <bonce_reloko@hotmail.com>
2020-09-12 00:40:49 -0300

    se agregaron al admin cada model

--- oficios/apps/Comentarios/views.py
+++ oficios/apps/Comentarios/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Comentarios
 # Create your views here.
+def Mostrar(request):
+    objetos= Comentarios.objects.all()
+    return render(request,'Comentarios/Mostrar.html',{'objetos':objetos})
