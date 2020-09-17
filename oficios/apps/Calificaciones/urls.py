0a5263dc5cf7bf85fa26d0971b39c09b310d9d59
fealegre <fealegre@hotmail.com>
2020-09-17 07:37:02 -0300

    1

--- oficios/apps/Calificaciones/urls.py
+++ oficios/apps/Calificaciones/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name='Calificaciones'
 
 urlpatterns = [
-    path('Calificar/', views.Calificar.as_view(), name = 'Calificar'),
+    path('Calificar/<str:pk>/', views.Calificar.as_view(), name = 'Calificar'),
 ]
--- oficios/apps/Trabajadores/models.py
+++ oficios/apps/Trabajadores/models.py
@@ -12,7 +12,7 @@ class Trabajadores(models.Model):
     usuario= models.OneToOneField(Usuarios,on_delete=models.CASCADE,related_name='Worker')
     promedio= models.DecimalField(max_digits=3,decimal_places=2,default=0)
     especialidad= models.CharField(max_length=50)
-    certificado = models.ImageField(upload_to='Trabajadores',null=True,blank=True)
+    #certificado = models.ImageField(upload_to='Trabajadores',null=True,blank=True)
     rubro = models.ForeignKey(Rubros,null=True,on_delete=models.SET_NULL,related_name='filtro_rubro')
     zonas = models.ManyToManyField(Zonas,related_name='Opera',blank=True)
 
