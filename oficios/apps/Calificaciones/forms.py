7841a00fbadbd4329290f5481d94b35554d0f4b9
BilaxyM <danielamaidana95@gmail.com>
2020-09-19 18:29:45 -0300

    Estrellitas bonitas + una varita a calificar

--- oficios/apps/Calificaciones/forms.py
+++ oficios/apps/Calificaciones/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, RadioSelect
 from .models import Calificaciones
 
 class CalificacionesForm(ModelForm):
     class Meta:
         model=Calificaciones
         fields=['calificacion','comentario']
+        widgets = {
+            'calificacion': RadioSelect(),
+        }
--- oficios/apps/Calificaciones/models.py
+++ oficios/apps/Calificaciones/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from ..Trabajadores.models import Trabajadores
 
-
+valores=[(1,1),(2,2),(3,3),(4,4),(5,5)]
 class Calificaciones(models.Model):
     trabajador = models.ForeignKey(Trabajadores,on_delete=models.CASCADE,related_name='calificado')
-    calificacion = models.PositiveSmallIntegerField()
+    calificacion = models.PositiveSmallIntegerField(choices=valores)
     comentario = models.TextField(default=None)
     
     def __str__(self):
