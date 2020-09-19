16dca6fa3243b343c0eb15f0473cb4556f3d8b77
Emanuel <EmanuelAleRojas@gmail.com>
2020-09-19 01:24:26 -0300

    Perfil del trabajador

--- oficios/apps/Trabajadores/urls.py
+++ oficios/apps/Trabajadores/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('Registro',views.Registro.as_view(),name='registro'),
     path('Listar',views.Listar.as_view(),name='Listar'),
     path('EditarPerfil/<str:pk>',views.EditarPerfil.as_view(),name='editarPerfil'),
+    path('MostrarPerfil/<str:pk>', views.MostrarPerfil.as_view(), name='mostrarPerfil'),
 ]
