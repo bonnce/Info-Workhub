6b8ae892ee19525b40966e70846b180c2d178350
Cristian Tomasi <cristiantomasi@gmail.com>
2020-09-23 23:34:32 -0300

    datatables en listar trabajadores

--- oficios/apps/Zonas/views.py
+++ oficios/apps/Zonas/views.py
@@ -1,10 +1,8 @@
 from django.views.generic.edit import CreateView 
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
 from ..Zonas.models import Zonas
 from django.urls import reverse_lazy
 from ..utils.funciones import PermisosMixin,PasajeMixin
-from .forms import ZonasForm
 from django.views.generic.list import ListView
 # Create your views here.
 
